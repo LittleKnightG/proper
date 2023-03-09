afac27dccbfa996ff9abc35f61abc2aeea4e497b
LittleKnightG <91250669+LittleKnightG@users.noreply.github.com>
2023-03-09 11:20:13 +0000

    Add files via upload

---
+++ prototype one.py
@@ -0,0 +1,395 @@
+# Step 1: create a variable score and set the value to 0
+score = 0
+# Step 2: Create a questions array called geography with 10 questions for students in year 11
+geography = [
+  "What is the capital of Australia?",
+  "What is the longest river in the world?",
+  "What is the largest ocean in the world?",
+  "What is the name of the largest desert in the world?",
+  "What is the name of the largest mountain range in the world?",
+  "What is the name of the largest country in the world?",
+  "What is the name of the smallest country in the world?",
+  "What is the name of the largest island in the world?",
+  "What is the name of the highest waterfall in the world?",
+  "What is the name of the deepest ocean trench in the world?"
+]
+# Step 3: create the answers to the geography
+geography_answers = [
+  "Canberra",
+  "Nile",
+  "Pacific Ocean",
+  "Sahara Desert",
+  "Andes",
+  "Russia",
+  "Vatican City",
+  "Greenland",
+  "Angel Falls",
+  "Mariana Trench"
+]
+# Step 4: Create a questions array called biology with 10 questions for students in year 11
+biology = [
+  "What is the scientific name for the study of plants?",
+  "What is the smallest unit of life called?",
+  "What is the name of the organ that filters the blood in the human body?",
+  "What is the name of the process that generates energy in organisms?",
+  "What is the name of the organ that pumps blood in the human body?",
+  "What is the name of the molecule that stores genetic information in cells?",
+  "What is the name of the process that allows cells to divide and make new cells?",
+  "What is the name of the main source of energy for living organisms?",
+  "What is the name of the structure that provides support and protection for plant cells?",
+  "What is the name of the substance in blood that carries oxygen to cells?"]
+# Step 5: create the answers to the biology
+biology_answers = [
+  "Botany",
+  "Cell",
+  "Kidneys",
+  "Respiration",
+  "Heart",
+  "DNA",
+  "Mitosis",
+  "Glucose",
+  "Cell wall",
+  "Haemoglobin"
+]
+# Step 6: Create a questions array called chemistry with 10 questions for students in year 11
+chemistry = [
+  "What is the name of the element with the atomic number 1?",
+  "What is the name of the process that converts a solid into a liquid?",
+  "What is the name of the chemical formula for water?",
+  "What is the name of the process that converts a liquid into a gas?",
+  "What is the name of the substance in a battery that produces electricity?",
+  "What is the name of the process that involves the formation of a new substance by chemical reaction?",
+  "What is the name of the type of bond that forms between two opositely charged ions ?",
+  "What is the name of the substance in vinegar that makes it sour?",
+  "What is the name of the type of reaction that releases energy?",
+  "What is the name of the chemical formula for table salt?"
+]
+
+# Step 7: create the answers to the chemistry
+chemistry_answers = [
+  "Hydrogen",
+  "Melting",
+  "H2O",
+  "Evaporation",
+  "Electrolyte",
+  "Synthesis",
+  "Ionic",
+  "Acetic acid",
+  "Exothermic",
+  "NaCl"
+]
+# Step 8: Create a questions array called physics with 10 questions for students in year 11
+physics = [
+  "What is the name of the force that acts upon an object that is stationary on a flat surface?",
+  "What is the name of the type of energy that is associated with motion?",
+  "What is the name of the device that measures force and is used to measure weight?",
+  "What is the name of the law that states that for every action, there is an equal and opposite reaction?",
+  "What is the name of the unit used to measure the amount of energy transferred or converted in a system?",
+  "What is the name of the phenomenon that occurs when a solid changes directly into a gas, bypassing the liquid phase?",
+  "What is the name of the type of energy that is stored in an object due to its position or shape?",
+  "What is the name of the type of energy that is associated with the movement of charged particles?",
+  "What is the name of the force that attracts two objects toward each other, proportional to their masses and distance apart?",
+  "What is the name of the speed of light in a vacuum?"
+]
+
+# Step 9: create the answers to the physics
+physics_answers = [
+  "Gravity",
+  "Kinetic energy",
+  "Scale",
+  "Newton's Third Law of Motion",
+  "Joule",
+  "Sublimation",
+  "Potential energy",
+  "Electrical energy",
+  "Gravitational force",
+  "c (299,792,458 meters per second)"
+]
+# Step 10: Create a questions array called computer science with 10 questions for students in year 11
+computer_science = [
+  "What is the name of the type of software that allows a user to create and edit digital media?",
+  "What is the name of the type of software that allows a user to manage and organize files on a computer?",
+  "What is the name of the type of software that provides a graphical user interface (GUI) for computer users?",
+  "What is the name of the type of software that enables communication over the internet?",
+  "What is the name of the type of software that allows a user to perform mathematical calculations?",
+  "What is the name of the programming language that was first introduced in the 1970s and is still widely used today?",
+  "What is the name of the type of software that provides security for computer systems?",
+  "What is the name of the type of software that allows a user to browse and search the internet?",
+  "What is the name of the type of software that is used for creating and editing documents?",
+  "What is the name of the type of software that provides a platform for running other software applications?"
+]
+
+# Step 11: create the answers to the computer science
+computer_science_answers = [
+  "Media editing software",
+  "File manager",
+  "Operating system",
+  "Internet browser",
+  "Calculator",
+  "C",
+  "Antivirus software",
+  "Web browser",
+  "Word processor",
+  "Operating system"
+]
+# Step 12: Create a questions array called economics with 10 questions for students in year 11
+economics = [
+  "What is the name of the type of economy where the government controls production and distribution of goods and services?",
+  "What is the name of the type of economy where individuals own and operate businesses to make a profit?",
+  "What is the name of the concept that states that individuals and businesses will make choices based on their self-interest?",
+  "What is the name of the concept that states that a good or service has a lower price if it is produced in large quantities and a higher price if it is produced in small quantities?",
+  "What is the name of the concept that states that prices will adjust to bring supply and demand into balance?",
+  "What is the name of the concept that states that people will buy more of a good or service if its price decreases, and buy less if its price increases?",
+  "What is the name of the type of economy where people can buy and sell goods and services freely and without government intervention?",
+  "What is the name of the economic system that combines elements of a market economy and a planned economy?",
+  "What is the name of the type of economy where the government provides goods and services and sets prices to achieve economic and social goals?",
+  "What is the name of the concept that states that people will work more if they are paid more, and work less if they are paid less?"
+]
+
+# Step 13: create the answers to the economics
+economics_answers = [
+  "Command economy",
+  "Market economy",
+  "Rational self-interest",
+  "Law of diminishing returns",
+  "Law of supply and demand",
+  "Law of demand",
+  "Free market economy",
+  "Mixed economy",
+  "Planned economy",
+  "Law of supply"
+]
+# Step 14: Create a questions array called English literature with 10 questions for students in year 11
+english_literature = [
+  "Who wrote the novel 'Pride and Prejudice' in the 19th century?",
+  "Who wrote the play 'Romeo and Juliet' in the 16th century?",
+  "Who wrote the novel 'To Kill a Mockingbird' in the 20th century?",
+  "Who wrote the novel 'Wuthering Heights' in the 19th century?",
+  "Who wrote the play 'Hamlet' in the early 17th century?",
+  "Who wrote the novel 'Great Expectations' in the 19th century?",
+  "Who wrote the novel 'Jane Eyre' in the 19th century?",
+  "Who wrote the play 'Macbeth' in the early 17th century?",
+  "Who wrote the novel 'Moby-Dick' in the 19th century?",
+  "Who wrote the play 'Othello' in the early 17th century?"
+]
+
+# Step 15: create the answers to the English language
+english_literature_answers = [
+  "Jane Austen",
+  "William Shakespeare",
+  "Harper Lee",
+  "Emily Bronte",
+  "William Shakespeare",
+  "Charles Dickens",
+  "Charlotte Bronte",
+  "William Shakespeare",
+  "Herman Melville",
+  "William Shakespeare"
+]
+# Step 16: Create a questions array called religious studies with 10 questions for students in year 11
+religious_studies = [
+  "What is the name of the holy book in Christianity?",
+  "What is the name of the holy book in Islam?",
+  "What is the name of the holy book in Hinduism?",
+  "What is the name of the holy book in Buddhism?",
+  "What is the name of the holy book in Sikhism?",
+  "What is the name of the founder of Christianity?",
+  "What is the name of the founder of Islam?",
+  "What is the name of the founder of Hinduism?",
+  "What is the name of the founder of Buddhism?",
+  "What is the name of the founder of Sikhism?"
+]
+
+# Step 17: create the answers to the religious studies
+religious_studies_answers = [
+  "Bible",
+  "Quran",
+  "Vedas",
+  "Tripitaka",
+  "Guru Granth Sahib",
+  "Jesus Christ",
+  "Prophet Muhammad",
+  "No single founder",
+  "Siddhartha Gautama",
+  "Guru Nanak"
+]
+# Step 18: Create a questions array called maths with 10 questions for students in year 11
+maths = [
+  "What is the formula for calculating the area of a circle?",
+  "What is the formula for calculating the volume of a sphere?",
+  "What is the formula for calculating the length of a hypotenuse in a right-angled triangle?",
+  "What is the formula for calculating the slope of a line?",
+  "What is the formula for calculating the midpoint of a line segment?",
+  "What is the formula for calculating the distance between two points?",
+  "What is the formula for calculating the equation of a line given two points?",
+  "What is the formula for calculating the equation of a circle given its center and radius?",
+  "What is the formula for calculating the determinant of a matrix?",
+  "What is the formula for calculating the inverse of a matrix?"
+]
+
+# Step 19: create the answers to the maths
+maths_answers = [
+  "πr^2",
+  "4/3 πr^3",
+  "√(a^2 + b^2)",
+  "m = (y2 - y1) / (x2 - x1)",
+  "((x1 + x2) / 2, (y1 + y2) / 2)",
+  "√((x2 - x1)^2 + (y2 - y1)^2)",
+  "y = mx + c",
+  "(x - h)^2 + (y - k)^2 = r^2",
+  "det(A)",
+  "A^-1"
+]
+# Step 20: Create a questions array called business studies with 10 questions for students in year 11
+business_studies = [
+  "What is the definition of economics?",
+  "What is the definition of a sole trader?",
+  "What is the definition of a partnership?",
+  "What is the definition of a limited company?",
+  "What is the definition of a franchise?",
+  "What is the definition of a joint venture?",
+  "What is the definition of market research?",
+  "What is the definition of a supply chain?",
+  "What is the definition of a balance sheet?",
+  "What is the definition of a cash flow statement?"
+]
+
+# Step 21: create the answers to the business studies
+business_studies_answers = [
+  "The study of how people use resources to meet their needs and wants",
+  "A type of business structure where one person owns and runs the business",
+  "A type of business structure where two or more people own and run the business together",
+  "A type of business structure where the owners have limited liability for the company's debts",
+  "A type of business relationship where the franchisee operates a business using the franchisor's brand and systems",
+  "A type of business partnership where two or more companies work together on a specific project or business venture",
+  "The process of gathering and analyzing information about a market, product, or service",
+  "The network of businesses and organizations involved in producing and delivering a product or service",
+  "A financial statement that shows a company's assets, liabilities, and equity at a specific point in time",
+  "A financial statement that shows a company's inflows and outflows of cash over a specific period of time"
+]
+# Step 22: Say hi to the user
+print("Hi! Welcome to the revision quiz.")
+
+# Step 23: Ask for their name
+name = input("What's your name? ")
+
+
+# Step 24: Ask the user what subject they want to revise for
+print("Which subject would you like to revise for today?")
+subject = input("Choose from: geography, biology, chemistry, physics, computer science, economics, English literature, religious studies, maths, or business studies: ")
+
+# Step 25: Ask the user how many questions they want to go over for the chosen subject
+number_of_questions = int(input("How many questions would you like to go over for " + subject + "? "))
+print("You have chosen " + str(number_of_questions) + " questions for " + subject + ".")
+print("Here are your questions:")
+
+# Step 26: Give the user the number of questions they have asked for in a random order
+import random
+score = 0
+if subject == "geography":
+    questions = geography
+    answers = geography_answers
+elif subject == "biology":
+    questions = biology
+    answers = biology_answers
+elif subject == "chemistry":
+    questions = chemistry
+    answers = chemistry_answers
+elif subject == "physics":
+    questions = physics
+    answers = physics_answers
+elif subject == "computer science":
+    questions = computer_science
+    answers = computer_science_answers
+elif subject == "economics":
+    questions = economics
+    answers = economics_answers
+elif subject == "English literature":
+    questions = english_literature
+    answers = english_literature_answers
+elif subject == "religious studies":
+    questions = religious_studies
+    answers = religious_studies_answers
+elif subject == "maths":
+    questions = maths
+    answers = maths_answers
+elif subject == "business studies":
+    questions = business_studies
+    answers = business_studies_answers
+else:
+    print("Sorry, that subject is not available.")
+
+random_questions = random.sample(questions, number_of_questions)
+for i in range(number_of_questions):
+    question = random_questions[i]
+    answer = answers[questions.index(question)]
+    user_answer = input(question + ": ")
+    if user_answer.lower() == answer.lower():
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect. The correct answer is " + answer + ".")
+
+# Step 28: At the end of the questions give the user their score
+print("You got " + str(score) + " out of " + str(number_of_questions) + " questions correct.")
+
+# Step 29: Ask the user if they want to do more questions
+while False:
+    response = input("Do you want to play again? (yes or no) ")
+    if response == "no":
+        print("Thanks for playing! Goodbye!")
+        break
+    elif response == "yes":
+        print("Great! Here we go again...")
+        # Have them do the questions
+        if subject == "geography":
+            questions = geography
+            answers = geography_answers
+        elif subject == "biology":
+            questions = biology
+            answers = biology_answers
+        elif subject == "chemistry":
+            questions = chemistry
+            answers = chemistry_answers
+        elif subject == "physics":
+            questions = physics
+            answers = physics_answers
+        elif subject == "computer science":
+            questions = computer_science
+            answers = computer_science_answers
+        elif subject == "economics":
+            questions = economics
+            answers = economics_answers
+        elif subject == "English literature":
+            questions = english_literature
+            answers = english_literature_answers
+        elif subject == "religious studies":
+            questions = religious_studies
+            answers = religious_studies_answers
+        elif subject == "maths":
+            questions = maths
+            answers = maths_answers
+        elif subject == "business studies":
+            questions = business_studies
+            answers = business_studies_answers
+    print(" not available")
+
+    random_questions = random.sample(questions, number_of_questions)
+    for i in range(number_of_questions):
+        question = random_questions[i]
+        answer = answers[questions.index(question)]
+        user_answer = input(question + ": ")
+        if user_answer.lower() == answer.lower():
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect. The correct answer is " + answer + ".")
+    else:
+        print("Not available. Please type 'yes' or 'no'.")
+    
+
+# At the end of the questions give the user their score
+print("You got " + str(score) + " out of " + str(number_of_questions) + " questions correct.")
+# Ask the user if they want to do more questions
+more_questions = input("Would you like to do more questions? (yes/no) ")
+
